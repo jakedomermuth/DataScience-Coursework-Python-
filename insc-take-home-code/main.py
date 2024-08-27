eb87fdd2bb7c653b68113f424b93e7908d3358f7
Jake Domermuth <146481362+jakedomermuth@users.noreply.github.com>
2024-08-27 16:14:47 -0400

    Update main.py

--- insc-take-home-code/main.py
+++ insc-take-home-code/main.py
@@ -1,30 +1,31 @@
-from other_games import draw_card, roll_dice, flip_coin
+from games import game_choice, flip_coin, roll_dice, draw_card
+#importing functions
+
 
 if __name__ == '__main__':
+    #running hub option and selecting/playing a game until the user decides to exit
     while True:
-        print('Welcome to the game hub')
-        game_input = input('Would you like to flip a coin(1), roll a dice(2), or draw a card(3):' )
+        print('Welcome to the game hub!')
+        game_input = input(
+            'Would you like to flip a coin (1), roll a dice (2), or draw a card (3)? Type "quit" to exit: ')
 
         if game_input == '1':
-            while True:
-                flip_result = flip_coin()
-                print(flip_result)
+            game_choice("Coin flip", flip_coin)
+            # creating a name for printing purposes and calling the correct game function
 
         elif game_input == '2':
-            while True:
-                dice_input = input('Would you like a 6, 8, 10, or 12 sided dice: ')
-                dice_result = roll_dice()
-                print(dice_result)
-
+            game_choice("Dice roll", roll_dice)
+            # creating a name for printing purposes and calling the correct game function
 
         elif game_input == '3':
-            while True:
-                card_input = input('Would you like a Joker? yes or no ')
-                card_result = draw_card(card_input)
-                print(card_result)
+            game_choice("Draw card", draw_card)
+            # creating a name for printing purposes and calling the correct game function
 
-        elif game_input == 'quit':
+        #Allowing user to exit the game hub
+        elif game_input.lower() == 'quit':
+            print('Thank you for playing!')
             break
 
         else:
+            print('Invalid Selection. Please enter 1, 2, 3, or quit')
             print('Invalid Selection. Please enter 1, 2, 3, or quit')
